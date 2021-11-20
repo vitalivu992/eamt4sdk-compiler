ad62e6fc2155368013637cea5377b55329baa802
Linh Vu Thai <vuthailinh.linh7.v7@gmail.com>
2021-11-21 00:27:34 +0700

    Change the default source folder to first arg

--- compile.sh
+++ compile.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-for f in `ls source/Experts|grep mq4|grep -v log`; do
-    WINEDEBUG=-all wine sdk/metaeditor.exe /compile:source/Experts/$f /include:source /log:output/$f.log
+
+echo "$#"
+if [[ $# -gt 0 ]]; then
+    source=$1
+else 
+    source="source"
+fi
+
+for f in `ls $source/Experts|grep mq4|grep -v log`; do
+    WINEDEBUG=-all wine /mt4/sdk/metaeditor.exe /compile:$source/Experts/$f /include:$source /log:/mt4/output/$f.log
 
     if [ $? -ne 1 ]; then # metaeditor return 1 if compiling is success
-        cat output/$f.log
+        cat /mt4/output/$f.log
         exit 1
     fi
 
     echo "\n---\nCompiled on `date`\nLV" >> $f.log 
 done
 
-find source/Experts -type f -name "*.ex4" -exec mv {} output \;
-find output -type f
+find $source/Experts -type f -name "*.ex4" -exec mv {} /mt4/output \;
+find /mt4/output -type f
 
 exit 0
