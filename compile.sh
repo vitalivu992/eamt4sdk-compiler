3677eaac05dca73d2fc75a019f76cb24c56ea419
Linh V <linhvu@fortna.com>
2022-02-05 16:50:53 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -12,19 +12,9 @@ for f in `ls $source/Experts|grep mq4|grep -v log`; do
     # cat $source/Experts/$f
     touch $source/Experts/$f.log
     WINEDEBUG=-all wine /mt4/sdk/metaeditor.exe /compile:$source/Experts/$f /log:$source/Experts/$f.log /include:$source
-    if [ $? -ne 0 ]; then # metaeditor return 1 if compiling is success
-        if [ -f $source/Experts/$f.log ]; then
-            cat $source/Experts/$f.log
-        fi
-        echo "----------------------------------------------"
-        echo "Failed when compiling EA $f. See the log above"
-        exit 1
-    else
-        echo "----------------------------------------------"
-        echo "Successfully compile EA $f"
+    if [ -f $source/Experts/$f.log ]; then
+        cat $source/Experts/$f.log
     fi
-
-    echo "\n---\nCompiled on `date`\nLV" >> $f.log 
 done
 
 find $source/Experts -type f
